4c95b6c293120fca250ab8f2546c258627d97a23
Gilberto Gonçalves <lursty@gmail.com>
2013-11-04 20:11:32 +0000

    Added setup.py and moved txt2bootstrap to bin/

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='txt2bootstrap',
+    version='1.0',
+    packages=[],
+    license='MIT',
+    long_description=open('README.md').read(),
+    scripts=['bin/txt2bootstrap'],
+    install_requires=[]
+)
