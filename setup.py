ee6fb89ccfd2ca1e10146f399ffb4e839058fb8f
Gilberto Gonçalves <lursty@gmail.com>
2013-11-10 12:12:29 +0000

    Upped the version to 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='txt2bootstrap',
-    version='1.0',
+    version='1.1',
     packages=[],
     license='MIT',
     long_description=open('README.rst').read(),
