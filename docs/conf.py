89919392df39357bc1cd29de6d7ec6efca61ddc5
Gil Gonçalves <lursty@gmail.com>
2014-06-09 10:27:00 +0100

    Updated copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'txt2bootstrap'
-copyright = u'2013, Gil Goncalves'
+copyright = u'2014, Gil Goncalves'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
