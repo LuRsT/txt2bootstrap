6ea53f64f4b42e0bb37ed74a70e4fd3ea41574d9
Gil Gonçalves <lursty@gmail.com>
2015-02-22 16:37:46 +0000

    Updated version to 2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='txt2bootstrap',
-    version='1.1',
+    version='2.0',
     packages=[],
     license='MIT',
     long_description=open('README.rst').read(),
